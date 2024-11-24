2440908a53964fa0418208a8da6639723a43fb5c
Paul Adams <nihaopaul@gmail.com>
2024-11-24 17:12:26 +0800

    Added: get_header_names Added: read_config

--- forward-auth-wasm/src/guest.rs
+++ forward-auth-wasm/src/guest.rs
@@ -1,4 +1,5 @@
 // reference code from https://github.com/elisasre/http-wasm-rust/blob/main/src/guest.rs
+use serde::{Deserialize, Serialize};
 use std::str;
 
 pub const FATAL: i32 = 3;
@@ -7,8 +8,8 @@ pub const WARN: i32 = 1;
 pub const INFO: i32 = 0;
 pub const DEBUG: i32 = -1;
 
-pub const REQUEST_HEADER: u32 = 0;
-pub const RESPONSE_HEADER: u32 = 1;
+pub const REQUEST_HEADER: i32 = 0;
+pub const RESPONSE_HEADER: i32 = 1;
 
 pub const REQUEST_BODY: u32 = 0;
 pub const RESPONSE_BODY: u32 = 1;
@@ -17,9 +18,18 @@ pub const FEATURE_BUFFER_REQUEST: u32 = 1;
 pub const FEATURE_BUFFER_RESPONSE: u32 = 2;
 pub const FEATURE_TRAILERS: u32 = 3;
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CloudflareConfig {
+    pub cf_domain: String,
+    pub cf_org: String,
+    pub cf_token: String,
+}
+
 #[link(wasm_import_module = "http_handler")]
 extern "C" {
+    // working with log
     fn log(level: i32, message: *const u8, message_len: u32);
+    // working with get_config
     fn get_config(buf: *const i32, buf_limit: i32) -> i32;
     fn get_method(buf: *const u8, buf_limit: i32) -> i32;
     fn set_method(ptr: *const u8, message_len: u32);
@@ -41,7 +51,9 @@ extern "C" {
         value_len: u32,
     );
     fn remove_header(header_kind: u32, name_ptr: *const u8, name_len: u32);
-    fn get_header_names(header_kind: u32, buf: *const u8, buf_limit: i32) -> i64;
+
+    // updated get_header_names signature
+    fn get_header_names(header_kind: i32, buf: *const i32, buf_limit: i32) -> i64;
     fn get_header_values(
         header_kind: u32,
         name_ptr: *const u8,
@@ -55,6 +67,7 @@ extern "C" {
     fn get_status_code() -> i32;
     fn set_status_code(code: i32);
     fn enable_features(feature: u32) -> i32;
+    // working with get source address
     fn get_source_addr(buf: *const u8, buf_limit: i32) -> i32;
 }
 
@@ -118,10 +131,32 @@ pub fn get_header_val(kind: u32, name: &str) -> Vec<String> {
     };
 }
 
-pub fn get_headers(kind: u32) -> Vec<String> {
-    let read_buf: [u8; 2048] = [0; 2048];
+pub fn get_headers(kind: i32) -> Vec<String> {
+    /*
+        ;; get_header_names writes all header names, in lowercase, NUL-terminated, to
+    ;; memory if the encoded length isn't larger than `buf_limit`. `count_len` is
+    ;; returned regardless of whether memory was written.
+    ;;
+    ;; Note: A host who fails to get header names will trap (aka panic,
+    ;; "unreachable" instruction).
+    (import "http_handler" "get_header_names" (func $get_header_names
+      (param $kind i32)
+      (param  $buf i32) (param $buf_limit i32)
+      (result (; count << 32| len ;) i64)))
+       */
     unsafe {
-        match get_header_names(kind, read_buf.as_ptr(), 2048) {
+        let result = get_header_names(kind, std::ptr::null(), 0);
+
+        let _count = (result >> 32) as i32;
+        let len = result as i32;
+
+        if len == 0 {
+            return Vec::new();
+        }
+
+        let read_buf = vec![0u8; len as usize];
+
+        match get_header_names(kind, read_buf.as_ptr() as *const i32, len) {
             len => {
                 let data: &[u8] = &read_buf[0..len as usize];
                 return str_array_from_u8_nul_utf8_unchecked(data);
@@ -173,15 +208,15 @@ pub fn get_conf() -> Vec<u8> {
 
         // Allocate buffer
         let mut buffer = vec![0u8; config_len as usize];
-        // Get actual config data
-        let len = get_config(buffer.as_ptr() as *const i32, config_len);
 
-        if len == 0 {
-            return Vec::new();
+        // Get actual config data
+        match get_config(buffer.as_ptr() as *const i32, config_len) {
+            len => {
+                // Resize buffer to actual length and return
+                buffer.truncate(len as usize);
+                buffer
+            }
         }
-        // Resize buffer to actual length and return
-        // buffer.truncate(len as usize);
-        buffer
     }
 }
 
--- forward-auth-wasm/src/lib.rs
+++ forward-auth-wasm/src/lib.rs
@@ -1,46 +1,40 @@
+use guest::CloudflareConfig;
 use lazy_static::lazy_static;
-use serde::{Deserialize, Serialize};
-use std::str;
-
 mod guest;
 
-// TODO: this function does not quite return the Config struct yet but the bytes of decimals do
+// Returns the Structured Configuration from the host in JSON format
 lazy_static! {
-    static ref CONFIG: Config = {
+    static ref CONFIG: CloudflareConfig = {
         match serde_json::from_slice(&guest::get_conf()) {
-            Ok(config) => config,
+            Ok(config) => {
+                guest::send_log(guest::DEBUG, format!("{:?}", config).as_str());
+                config
+            }
             Err(e) => {
+                let empty_config = CloudflareConfig {
+                    cf_domain: "NO_DOMAIN_DEFINED".to_string(),
+                    cf_org: "NO_ORG_DEFINED".to_string(),
+                    cf_token: "NO_TOKEN_DEFINED".to_string(),
+                };
                 guest::send_log(guest::WARN, format!("{:?}", e).as_str());
-                guest::send_log(guest::WARN, format!("{:?}", &guest::get_conf()).as_str());
-                Config {
-                    CF_DOMAIN: "NO_DOMAIN".to_string(),
-                    CF_ORG: "NO_ORG".to_string(),
-                    CF_TOKEN: "NO_TOKEN".to_string(),
-                }
+                guest::send_log(guest::WARN, format!("{:?}", empty_config).as_str());
+                empty_config
             }
         }
     };
 }
 
-fn main() {}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Config {
-    CF_DOMAIN: String,
-    CF_ORG: String,
-    CF_TOKEN: String,
-}
-
 #[export_name = "handle_request"]
 pub fn http_request() -> i64 {
-    let conf: &Config = &*CONFIG;
-    // for (k, v) in &conf.headers {
-    //     guest::add_header(guest::REQUEST_HEADER, &k, &v);
-    // }
-    // guest::send_log(guest::DEBUG, format!("{:?}", guest::get_addr()).as_str());
-    guest::send_log(guest::DEBUG, format!("{:?}", conf).as_str());
+    // let conf: &CloudflareConfig = &*CONFIG;
+
+    let headers = &guest::get_headers(guest::REQUEST_HEADER);
+
+    guest::send_log(guest::DEBUG, format!("{:?}", headers).as_str());
     return 16 << 32 | 1 as i64;
 }
 
 #[export_name = "handle_response"]
-fn http_response(_req_ctx: i32, _is_error: i32) {}
+fn http_response(_req_ctx: i32, _is_error: i32) {
+    guest::send_log(guest::INFO, format!("{:?}", _req_ctx).as_str())
+}
